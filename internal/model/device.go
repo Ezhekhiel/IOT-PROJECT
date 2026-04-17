9af3f9876eb886b3c31dbf7e8e092c2acc93d2ad
Ezhekiel <lean.simulation.pwi2@gmail.com>
2026-04-17 09:42:03 +0700

    feat: add device and sensor models

--- internal/model/device.go
+++ internal/model/device.go
@@ -1,8 +1,8 @@
 package model
 
 type Device struct {
-	ID         int
-	DeviceCode string
-	CellID     int
-	ProcessID  int
+	ID         int    `gorm:"column:id;primaryKey"`
+	DeviceCode string `gorm:"column:device_code"`
+	CellID     int    `gorm:"column:cell_id"`
+	ProcessID  int    `gorm:"column:process_id"`
 }
--- internal/model/sensor.go
+++ internal/model/sensor.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type SensorData struct {
-	ID          int64
-	DeviceID    int
-	Pressure    *float64
-	Temperature *float64
-	Timer       *int
-	CreatedAt   time.Time
+	ID          int64     `gorm:"column:id;primaryKey"`
+	DeviceID    int       `gorm:"column:device_id"`
+	Pressure    *float64  `gorm:"column:pressure"`
+	Temperature *float64  `gorm:"column:temperature"`
+	Timer       *int      `gorm:"column:timer"`
+	CreatedAt   time.Time `gorm:"column:created_at"`
 }
